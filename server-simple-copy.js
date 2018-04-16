dfb73fa2f1d8cee7097d821e85861d77b9fbb2c4
370455671 <37067601+370455671@users.noreply.github.com>
2018-04-17 00:10:08 +0800

    Update server-simple-copy.js

--- server-simple-copy.js
+++ server-simple-copy.js
@@ -2,34 +2,37 @@ var http = require('http')
 var fs = require('fs')
 var url = require('url')
 
-var server = http.createServer(function(req,res){
 
-var pathObj = url.parse(req.url,true)
-console.log(pathObj)
 
-switch(pathObj.pathname){
+http.createServer(function(req, res){
+
+  var pathObj = url.parse(req.url, true)
+  console.log(pathObj)
+
+  switch (pathObj.pathname) {
     case '/getWeather':
-    var ret
-    if(pathObj.query.city == 'beijing'){
+      var ret
+      if(pathObj.query.city == 'beijing'){
         ret = {
-            city: 'beijing',
-            weather: 'sunday'
+          city: 'beijing',
+          weather: '晴天'
         }
-    }else{
+      }else{
         ret = {
-            city: pathObj.query.city,
-            weather: 'unknow'
+          city: pathObj.query.city,
+          weather: '不知道'
         }
-    }
-    res.end(JSON.stringify(ret))
-        break;
-
+      }
+      res.end(JSON.stringify(ret))
+      break;
     case '/user/123':
-        res.end(fs.readFileSync(__dirname + '/static/user.tpl'))
-        break;
-    default:
-        res.end(fs.readFileSync(__dirname + '/static'+pathObj.pathname))
 
- }
-})
-server.listen(9000)
+      res.end( fs.readFileSync(__dirname + '/static/user.tpl' ))
+      break;
+    default:
+      res.end( fs.readFileSync(__dirname + '/static' + pathObj.pathname) )
+  }
+}).listen(8080)
+
+
+
